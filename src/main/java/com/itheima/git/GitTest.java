2b65a5eab2f01052081b6c50bc42b2f5a3b22453
4pk <1820302293@qq.com>
2022-07-23 11:30:29 +0800

    second commit

--- src/main/java/com/itheima/git/GitTest.java
+++ src/main/java/com/itheima/git/GitTest.java
@@ -3,5 +3,7 @@ package com.itheima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git !!!");
+        System.out.println("hello git222 !!!");
+//        蓝色代表追踪过又修改了
     }
 }
